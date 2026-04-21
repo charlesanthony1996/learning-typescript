9fdfe843a6c46761cd8067bf6f6bb41571fb7137
Charles Anthony <charlesanthony296@gmail.com>
2026-04-21 15:00:06 +0200

    read only properties

--- chapter_7.ts
+++ chapter_7.ts
@@ -1,2 +1,44 @@
 // interfaces
 
+// type Poet = {
+//     born: number,
+//     name: string
+// }
+
+
+// equivalent syntax for an interface
+interface Poet {
+    born: number
+    name: string
+}
+
+let valueLater: Poet
+
+valueLater = {
+    born: 1935,
+    name: 'Sara Teasdale'
+}
+
+// valueLater1 = "Emily Dickinson"
+
+// valueLater = {
+//     born: true,
+//     name: 'Sappho'
+// }
+
+// gives an error since born is not a boolean
+
+interface Book {
+    author?: string
+    pages: number
+}
+
+const ok: Book = {
+    author: "Rita Dove", 
+    pages: 80
+}
+
+const missing: Book = {
+    pages: 80
+}
+
