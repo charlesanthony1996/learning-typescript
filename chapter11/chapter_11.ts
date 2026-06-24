3650a2dd5f0aed0a72094914fd50172514fb723f
Charles Anthony <charlesanthony296@gmail.com>
2026-06-24 22:06:09 +0200

    declaration files

---
+++ chapter11/chapter_11.ts
@@ -0,0 +1,11 @@
+// declaration files
+
+import { Character } from "./types";
+
+export const charachter: Character ={
+    catchphrase: "hello mello",
+    name: "charles"
+}
+
+// console.log(charachter)
+
---
+++ chapter_1.ts
@@ -0,0 +1,1 @@
+// from js to ts
