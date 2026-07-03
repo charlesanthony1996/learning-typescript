0ac81cdc7aace3d2134351c8622edbed685ff96e
Charles Anthony <charlesanthony296@gmail.com>
2026-07-04 01:18:37 +0200

    generics

---
+++ chapter_10.ts
@@ -0,0 +1,18 @@
+// generics
+
+// function identity(input) {
+//     return input
+// }
+
+
+// errors.
+// has no type
+// identity("abc")
+// identity(123)
+// identity({ quote: "i think your self emerges more clearly over time"})
+
+
+function identity(input: number) {
+    return input
+}
+
