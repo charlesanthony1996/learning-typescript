ddac09a787e43e809cc311c92420b2d02b0490b4
Charles Anthony <charlesanthony296@gmail.com>
2026-07-04 01:39:12 +0200

    explicit generic call types

--- chapter_10.ts
+++ chapter_10.ts
@@ -12,7 +12,43 @@
 // identity({ quote: "i think your self emerges more clearly over time"})
 
 
-function identity(input: number) {
-    return input
-}
+// function identity(input: any) {
+//     return input
+// }
+
+// let value = identity(42)
+
+// generic functions
+
+// function identity<T>(input: T) {
+//     return input
+// }
+
+// const numeric = identity("me")
+// console.log(numeric)
+
+// const stringy = identity(123)
+// console.log(stringy)
+
+const identity = <T>(input: T) => input
+
+identity(123)
+identity("charles")
+
+function logWrapper<Input>(callback: (input: Input) => void) {
+    return (input: Input) => {
+        console.log("input: ", input)
+        callback(input)
+    }
+} 
+
+logWrapper((input: "string") => {
+    console.log(input.length)
+})
+
+// no input here. so gives an error
+// logWrapper((input) => {
+//     console.log(input.length)
+// })
+
 
